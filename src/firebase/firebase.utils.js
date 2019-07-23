01bdab9d98c0c3ebd8733c02bff36a32fcf6e30f
dre <spinninghalf@gmail.com>
2019-07-23 16:42:06 +1000

    remove console.logs

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,7 +18,7 @@ export const createUserProfileDocument = async (userAuth, additionalData ) => {
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
-  console.log('snapShot is: ',snapShot);
+  // console.log('snapShot is: ',snapShot);
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
