f699fe45f5de9f52fe4dfe23dcbab9be2b4dc716
dre <spinninghalf@gmail.com>
2019-07-28 09:23:32 +1000

    separating our cra app into client folder, added express server to help handle backend stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_SePZ50ccoRtmjva31b58fWAw00KVfgR7Tk';
   
   const onToken = token => {
+    // token allows stripe to know who is making the request,
+    // and where it's coming from
     console.log(token);
-    alert('payment successful')
+
+    // axios returns a promise
+    axios({
+      url: 'payment', // request to our *own* url route
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+    .then(response => {
+      alert('payment successful');
+    }).catch(error => {
+      console.log('payment error: ', JSON.parse(error));
+
+      // must use the test credit card, for now.
+      alert('there was an issue with your payment. please make sure you use the provided credit card');
+    });
   }
 
   return (
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+  // during dev/test our secret keys are accessed via this
+  // 
+  // in production, we've uploaded the secret keys to the server
+  // env manually via eg ssh connection to AWS instance
+  require('dotenv').config();
+}
+
+// stripe needs its secre key value. so we load stripe AFTER we've loaded
+// the key via dotenv (for dev). in prod, it will already be in the env
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+
+// heroku will put the port number at PORT env variable, 
+// otherwise hardcode the value to be 5000
+const port = process.env.PORT || 5000;
+
+// process any requests body parts into JSON
+app.use(bodyParser.json());
+
+// make sure the url string we get, or pass out,
+// are properly parsed. ie no spaces in url string
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow different origins
+// eg hosting a server on google. if another website makes
+// a request from a different origin, say amazon, then
+// by default a server will drop the request.
+// if you allow your app to use cors, then these types
+// of requests will be allowed by the google server.
+//
+// cors also applies to making requests from localhost client
+// to localhost server which will be on different *ports*.
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // for any url the user hits
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd',
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeResponse) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeResponse });
+    }
+  });
+});
